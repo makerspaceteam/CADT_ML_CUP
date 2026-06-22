812219dea38ddff71676a3079f5c8f63d8531759
Zorina69 <soychanratana14@gmail.com>
2026-06-22 13:15:18 +0700

    update gitignore

---
+++ shared/config.js
@@ -0,0 +1,7 @@
+// shared/config.js
+const CONFIG = {
+  SUPABASE_URL: 'https://rkhsxqeaebhhqywnuqmf.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJraHN4cWVhZWJoaHF5d251cW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNDI1MjgsImV4cCI6MjA5NjgxODUyOH0.6Q5SnXKkRkUfRs66FMBG-CWPh58RLWGM1FSXq93dRjY'
+};
+
+window.CONFIG = CONFIG;   // ← Important for non-module
